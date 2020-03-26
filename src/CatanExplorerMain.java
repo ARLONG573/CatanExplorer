eb4867a913584ebef97af8e34bd26b02146c0f9a
Aaron Tetens <tetena573@gmail.com>
2020-03-25 21:59:04 -0400

    eclipse project setup

---
+++ src/CatanExplorerMain.java
@@ -0,0 +1,12 @@
+/**
+ * Main class for the CatanExplorer program.
+ * 
+ * @author Aaron Tetens
+ */
+public class CatanExplorerMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
