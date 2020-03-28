a717e8235c5b6f51b6b51a07626e60a4dd202a20
Aaron Tetens <tetena573@gmail.com>
2020-03-28 16:26:48 -0400

    LaunchButtonPanel base complete, just need to implemented action listener

--- src/startup/players/LaunchButtonPanel.java
+++ src/startup/players/LaunchButtonPanel.java
@@ -1,23 +1,35 @@
 package startup.players;
 
-import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
  * This is the panel that holds the button that the user clicks to leave the
- * startup menu and enter the board editor.
+ * startup menu and enter the board editor. The button's action listener is in
+ * charge of validating the user data.
  * 
  * @author Aaron Tetens
  */
 public class LaunchButtonPanel extends JPanel {
 
 	private static final long serialVersionUID = 261221856020394181L;
+	private static final String LAUNCH_BUTTON_TEXT = "Launch";
+
+	private final JButton launchButton;
 
 	private static LaunchButtonPanel theInstance;
 
 	private LaunchButtonPanel() {
-		// TODO
-		super.add(new JLabel("LaunchButtonPanel"));
+		super(new BorderLayout());
+
+		this.launchButton = new JButton(LAUNCH_BUTTON_TEXT);
+		this.launchButton.addActionListener((e) -> {
+			// TODO
+		});
+
+		super.add(this.launchButton, BorderLayout.EAST);
 	}
 
 	/**
