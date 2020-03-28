33b68068323f1f35c6fe4933a0336bc8645822f5
Aaron Tetens <tetena573@gmail.com>
2020-03-28 15:10:04 -0400

    PlayerEntryPanel base done

--- src/startup/players/PlayerEntryPanel.java
+++ src/startup/players/PlayerEntryPanel.java
@@ -1,7 +1,12 @@
 package startup.players;
 
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import data.PlayerColor;
 
 /**
  * This is the panel that holds all of the components necessary for entering
@@ -12,8 +17,30 @@ import javax.swing.JPanel;
 class PlayerEntryPanel extends JPanel {
 
 	private static final long serialVersionUID = 5947952408283942209L;
+	private static final String NAME_LABEL_TEXT = "Name ";
+	private static final int DEFAULT_TEXT_FIELD_COLUMNS = 10;
+
+	private final JLabel nameLabel;
+	private final JTextField nameField;
+	private final JComboBox<PlayerColor> colorComboBox;
 
 	PlayerEntryPanel() {
-		super.add(new JLabel("PlayerEntryPanel"));
+		super.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+		this.nameLabel = new JLabel(NAME_LABEL_TEXT);
+		this.nameField = new JTextField(DEFAULT_TEXT_FIELD_COLUMNS);
+		this.colorComboBox = new JComboBox<>(PlayerColor.class.getEnumConstants());
+
+		super.add(this.nameLabel);
+		super.add(this.nameField);
+		super.add(this.colorComboBox);
+	}
+
+	String getPlayerName() {
+		return this.nameField.getText();
+	}
+
+	PlayerColor getPlayerColor() {
+		return (PlayerColor) (this.colorComboBox.getSelectedItem());
 	}
 }
