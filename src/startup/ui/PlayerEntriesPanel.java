cf1c7d1721e68e96f0787cba3bdb5f4a4543c6c2
Aaron Tetens <tetena573@gmail.com>
2020-03-28 13:58:17 -0400

    PlayerEntriesPanel#setNumPlayers done

--- src/startup/ui/PlayerEntriesPanel.java
+++ src/startup/ui/PlayerEntriesPanel.java
@@ -1,6 +1,9 @@
 package startup.ui;
 
-import javax.swing.JLabel;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 /**
@@ -12,12 +15,17 @@ import javax.swing.JPanel;
 class PlayerEntriesPanel extends JPanel {
 
 	private static final long serialVersionUID = -6940232339738566884L;
+	private static final int DEFAULT_NUM_PLAYERS = 4;
+	private static final String NUM_PLAYERS_ERROR = "Number of players must be 3 or 4";
+
+	private final List<PlayerEntryPanel> playerEntryPanels;
 
 	private static PlayerEntriesPanel theInstance;
 
 	private PlayerEntriesPanel() {
-		// TODO
-		super.add(new JLabel("PlayerEntriesPanel"));
+		super.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.playerEntryPanels = new ArrayList<>();
+		this.setNumPlayers(DEFAULT_NUM_PLAYERS);
 	}
 
 	/**
@@ -29,5 +37,52 @@ class PlayerEntriesPanel extends JPanel {
 		}
 
 		return theInstance;
+	}
+
+	/**
+	 * Sets the number of player entry panels to numPlayers. This method tries its
+	 * best to retain saved player information by only adding/removing the necessary
+	 * panels.
+	 * 
+	 * @param numPlayers
+	 *            The number of player entry panels to be displayed
+	 * @throws IllegalArgumentException
+	 *             If numPlayers is not in the range [3, 4]
+	 */
+	final void setNumPlayers(final int numPlayers) throws IllegalArgumentException {
+		if (numPlayers < 3 || numPlayers > 4) {
+			throw new IllegalArgumentException(NUM_PLAYERS_ERROR);
+		}
+
+		final int currentNumPlayers = this.playerEntryPanels.size();
+
+		if (currentNumPlayers < numPlayers) {
+			final int numPlayersToAdd = numPlayers - currentNumPlayers;
+			int numPlayersAdded = 0;
+
+			while (numPlayersAdded < numPlayersToAdd) {
+				final PlayerEntryPanel panelToAdd = new PlayerEntryPanel();
+
+				super.add(panelToAdd);
+				this.playerEntryPanels.add(panelToAdd);
+
+				numPlayersAdded++;
+			}
+		} else if (currentNumPlayers > numPlayers) {
+			final int numPlayersToRemove = currentNumPlayers - numPlayers;
+			int numPlayersRemoved = 0;
+
+			while (numPlayersRemoved < numPlayersToRemove) {
+				final PlayerEntryPanel panelToRemove = this.playerEntryPanels.get(this.playerEntryPanels.size() - 1);
+
+				super.remove(panelToRemove);
+				this.playerEntryPanels.remove(panelToRemove);
+
+				numPlayersRemoved++;
+			}
+		}
+
+		super.revalidate();
+		super.repaint();
 	}
 }
---
+++ src/startup/ui/PlayerEntryPanel.java
@@ -0,0 +1,19 @@
+package startup.ui;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * This is the panel that holds all of the components necessary for entering
+ * information about a single player.
+ * 
+ * @author Aaron Tetens
+ */
+class PlayerEntryPanel extends JPanel {
+
+	private static final long serialVersionUID = 5947952408283942209L;
+
+	PlayerEntryPanel() {
+		super.add(new JLabel("PlayerEntryPanel"));
+	}
+}
