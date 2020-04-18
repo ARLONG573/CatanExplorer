c3e905f6ec2b6e840e60a24167bb4b2b90434fdb
Aaron Tetens <tetena573@gmail.com>
2020-04-18 15:19:26 -0400

    PortType

---
+++ src/startup/board/data/PortType.java
@@ -0,0 +1,35 @@
+package startup.board.data;
+
+import java.awt.Color;
+
+/**
+ * This is an enumeration of the port types that can be selected for the startup
+ * board editor.
+ * 
+ * @author Aaron Tetens
+ */
+enum PortType implements Selectable {
+	WOOD_PORT("Wood Port", HexResource.WOOD.getBackgroundColor()), BRICK_PORT("Brick Port",
+			HexResource.BRICK.getBackgroundColor()), SHEEP_PORT("Sheep Port",
+					HexResource.SHEEP.getBackgroundColor()), WHEAT_PORT("Wheat Port",
+							HexResource.WHEAT.getBackgroundColor()), ORE_PORT("Ore Port",
+									HexResource.ORE.getBackgroundColor()), THREE_TO_ONE_PORT("3:1 Port", Color.WHITE);
+
+	private final String name;
+	private final Color color;
+
+	private PortType(final String name, final Color color) {
+		this.name = name;
+		this.color = color;
+	}
+
+	@Override
+	public Color getBackgroundColor() {
+		return this.color;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
