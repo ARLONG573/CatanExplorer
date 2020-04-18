ed2d25f121fbc9c2818463e2cc9b4844821369f2
Aaron Tetens <tetena573@gmail.com>
2020-04-18 15:40:43 -0400

    SelectableButton

--- src/startup/board/ui/SelectableButton.java
+++ src/startup/board/ui/SelectableButton.java
@@ -1,24 +1,26 @@
-package startup.board.ui.selectable;
+package startup.board.ui;
 
 import javax.swing.JButton;
 
 import startup.board.data.selectable.Selectable;
 
 /**
- * This is the superclass that defines the general behavior for the selectable
- * buttons in the startup board editor. This class's subclasses are responsible
- * for their visual representations.
+ * This is a button that the users clicks to select the board element that they
+ * want to add to the startup board editor.
  * 
  * @author Aaron Tetens
  */
-public class SelectableButton extends JButton {
+class SelectableButton extends JButton {
 
 	private static final long serialVersionUID = 8049570099616321459L;
 
 	private final Selectable selectable;
 
-	public SelectableButton(final Selectable selectable) {
+	SelectableButton(final Selectable selectable) {
 		this.selectable = selectable;
+
+		super.setBackground(this.selectable.getBackgroundColor());
+		super.setText(this.selectable.toString());
 
 		super.addActionListener((e) -> System.out.println("Selected " + this.selectable));
 	}
