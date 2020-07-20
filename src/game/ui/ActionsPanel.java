3eddf645c16256a87abc7e4d249ecd74cbeda2f2
Aaron Tetens <tetena573@gmail.com>
2020-07-20 19:51:31 -0400

    ActionsPanel layout is done

--- src/game/ui/ActionsPanel.java
+++ src/game/ui/ActionsPanel.java
@@ -1,7 +1,16 @@
 package game.ui;
 
-import javax.swing.JLabel;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import game.state.board.Board;
+import game.state.player.Player;
 
 /**
  * This panel contains the buttons that allows the user to input game actions as
@@ -13,12 +22,48 @@ import javax.swing.JPanel;
 class ActionsPanel extends JPanel {
 
 	private static final long serialVersionUID = 271961705838330478L;
+	private static final String ROLL_DICE_BUTTON_TEXT = "Roll dice";
+	private static final String TRADE_BUTTON_TEXT = "Trade";
+	private static final String BUILD_BUTTON_TEXT = "Build";
+	private static final String PLAY_DEV_CARD_BUTTON_TEXT = "Play dev card";
+	private static final int PADDING = 10;
 
 	private static ActionsPanel theInstance;
 
+	private final JButton rollDiceButton;
+	private final JButton tradeButton;
+	private final JButton buildButton;
+	private final JButton playDevCardButton;
+	private final JTextArea gameLogArea;
+
 	private ActionsPanel() {
-		// TODO add layout and actionless buttons
-		super.add(new JLabel("ACTIONS PANEL"));
+		this.rollDiceButton = new JButton(ROLL_DICE_BUTTON_TEXT);
+		this.rollDiceButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		this.tradeButton = new JButton(TRADE_BUTTON_TEXT);
+		this.tradeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		this.buildButton = new JButton(BUILD_BUTTON_TEXT);
+		this.buildButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		this.playDevCardButton = new JButton(PLAY_DEV_CARD_BUTTON_TEXT);
+		this.playDevCardButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		this.gameLogArea = new JTextArea();
+		this.gameLogArea.setEditable(false);
+
+		super.setPreferredSize(new Dimension(4 * Player.PAINT_WIDTH - Board.PAINT_SIZE, 0));
+		super.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		super.add(Box.createRigidArea(new Dimension(0, PADDING)));
+		super.add(this.rollDiceButton);
+		super.add(Box.createRigidArea(new Dimension(0, PADDING)));
+		super.add(this.tradeButton);
+		super.add(Box.createRigidArea(new Dimension(0, PADDING)));
+		super.add(this.buildButton);
+		super.add(Box.createRigidArea(new Dimension(0, PADDING)));
+		super.add(this.playDevCardButton);
+		super.add(Box.createRigidArea(new Dimension(0, PADDING)));
+		super.add(this.gameLogArea);
 	}
 
 	/**
